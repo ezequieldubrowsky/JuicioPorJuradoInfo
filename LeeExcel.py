4d7639c62d124cf0817f6ac4f57255c606a1b903
fancer1447 <165047112+ezequieldubrowsky@users.noreply.github.com>
2024-05-21 12:26:14 -0300

    Inicio
    -Entorno virtual
    -librerías necesarias

---
+++ LeeExcel.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import random
+datos = pd.read_excel("/Users/ezequieldubrowsky/Documents/programacion/Ruleta aleatoria/alumnos.xlsx")
+print(datos)
+def ImportarDatos():
+    pass
+def Seleccion():
+    pass
