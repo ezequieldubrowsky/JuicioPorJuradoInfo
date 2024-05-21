03b99e54ca7c29adf6941db129a1377ccd219d3c
fancer1447 <165047112+ezequieldubrowsky@users.noreply.github.com>
2024-05-21 13:24:13 -0300

    1.0
    Ahora lee el excel y lo convierte en un diccionario con key(nombre) y value(DNI)

--- LeeExcel.py
+++ LeeExcel.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import random
-datos = pd.read_excel("/Users/ezequieldubrowsky/Documents/programacion/Ruleta aleatoria/alumnos.xlsx")
-print(datos)
+datos = pd.read_excel("/Users/ezequieldubrowsky/Documents/programacion/Repos/JuicioPorJuradoInfo/alumnosfilas.xlsx")
+filas_como_dict = datos.to_dict(orient='records')
+
+# Imprime las filas como diccionarios (opcional)
+for fila_dict in filas_como_dict:
+    print(fila_dict)
+
+
 def ImportarDatos():
     pass
 def Seleccion():
